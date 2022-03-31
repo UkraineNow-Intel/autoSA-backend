54d6fddc6a367fe51ea57f06fc49cc714ef4c6b7
Owen Powell <owenpowell@Owens-MacBook-Pro.local>
2022-03-31 11:07:37 -0400

    Function for scraping live feed from liveuamap.com. Dataframe returns for each function.

--- infotools/webscraper.py
+++ infotools/webscraper.py
@@ -1,5 +1,10 @@
 """
-Use BeautifulSoup to scrape relevant websites with news pertaining to Ukraine
+Use BeautifulSoup to scrape relevant websites with news pertaining to Ukraine.
+
+Each function should return a dataframe holding the relevant information for each source, which can be
+accessed by the front end.
+
+Dataframe will also be saved to a CSV file found in /infotools/CSVs/scraped.csv
 """
 
 import pandas as pd
@@ -8,8 +13,8 @@ import requests
 import re
 
 
-#
-def get_from_nbc():
+def get_latest_from_nbc():
+    global content
     url = 'https://www.nbcnews.com/world/russia-ukraine-news'
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -17,10 +22,53 @@ def get_from_nbc():
     headlines = soup.findChildren('h2', text=re.compile(r'Ukraine'), limit=8)
 
     for i in range(len(headlines)):
-        print(headlines[i].text)
+        content = headlines[i].text
 
     nbc_data = pd.DataFrame(headlines)
     nbc_data.to_csv('infotools/CSVs/scraped.csv')
 
+    return nbc_data
 
-get_from_nbc()
+
+# result of this will need to be translated to english
+def get_latest_from_telegraf():
+    global content
+    url = 'https://telegraf.com.ua/specials/voyna-na-donbasse'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    headlines = soup.find_all('div', {'class': 'c-card-list__title'}, limit=5)
+
+    for i in range(len(headlines)):
+        content = headlines[i].text
+
+    telegraf_data = pd.DataFrame(headlines)
+    telegraf_data.to_csv('infotools/CSVs/scraped.csv')
+
+    return telegraf_data
+
+
+def get_from_liveuamap():
+    url = 'https://liveuamap.com'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    items = soup.select('div.news-lent div.event')
+
+    item_dicts = []
+    for item in items:
+        text = item.select('div.title')[0].text
+        item_dicts.append({
+            "interface": "website",
+            "source": "liveuamap.com",
+            "id": item.attrs['data-id'],
+            "headline": None,
+            "text": text,
+            "language": "en",
+            "timestamp": None,  # theoretically information is there e.g.<span class="date_add">1 hour ago</span>
+        })
+
+    liveuamap_data = pd.DataFrame(item_dicts)
+    liveuamap_data.to_csv('infotools/CSVs/scraped.csv')
+
+    return liveuamap_data
