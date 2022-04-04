0a6b3773b29c3f22e691063b81dc4829a9c1cef0
Ubuntu <ubuntu@ip-172-31-2-175.ec2.internal>
2022-04-04 08:39:00 +0000

    Added aws server config

---
+++ website/settings/prodaws.py
@@ -0,0 +1,32 @@
+from decouple import config
+from .base import *
+
+SECRET_KEY = config("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "MYAPP": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
