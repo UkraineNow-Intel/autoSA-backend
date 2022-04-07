8a4458d14ba7e3927dcb19aec546c585a054d698
Irina Truong <i.chernyavska@gmail.com>
2022-04-06 22:10:01 -0700

    Dummy periodic task.

--- website/celery.py
+++ website/celery.py
@@ -2,23 +2,15 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.dev")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings.dev')
 app = Celery("website")
-
-app.config_from_object("django.conf:settings")
-# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task
-def test(arg):
-    print(arg)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
+    import api.tasks
     sender.add_periodic_task(
-        crontab(minute="*/15"), test.s("hello"), name="add every 10"
+        crontab(minute="*", hour="*"), api.tasks.dummy_task.s("hello"), name="api.tasks.dummy_task"
     )
--- website/settings/base.py
+++ website/settings/base.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "storages",
     "django_extensions",
+    "django_celery_beat",
     "taggit",
     "rest_framework",
     "api.apps.ApiConfig",
@@ -125,9 +126,10 @@ TAGGIT_CASE_INSENSITIVE = True
 APPEND_SLASH = False
 
 # CELERY STUFF
-BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "UTC"
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
