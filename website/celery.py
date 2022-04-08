a7cabfb2f7cf4aa7d57bb109bb504199c39bdf99
Irina Truong <i.chernyavska@gmail.com>
2022-04-07 20:46:07 -0700

    Black.

--- website/celery.py
+++ website/celery.py
@@ -2,7 +2,7 @@ from celery import Celery
 from celery.schedules import crontab
 
 app = Celery("website")
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 
