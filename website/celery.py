fe80291713ef5b1f6782d1f1caed9f8a0993413e
Irina Truong <i.chernyavska@gmail.com>
2022-04-07 21:07:17 -0700

    Black & lint helper.

--- website/celery.py
+++ website/celery.py
@@ -10,7 +10,10 @@ app.autodiscover_tasks()
 def setup_periodic_tasks(sender, **kwargs):
     # schedule a dummy task to run every minute:
     # import api.tasks
+    #
     # sender.add_periodic_task(
-    #     crontab(minute="*", hour="*"), api.tasks.dummy_task.s("hello"), name="api.tasks.dummy_task"
+    #     crontab(minute="*", hour="*"),
+    #     api.tasks.dummy_task.s("hello"),
+    #     name="api.tasks.dummy_task",
     # )
     pass
