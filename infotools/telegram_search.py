0494361eab719988733ba590138d464076646620
Robin Tibor Schirrmeister <robintibor@gmail.com>
2022-04-13 01:34:57 +0200

    First attempt telegram search

--- infotools/telegram_search.py
+++ infotools/telegram_search.py
@@ -1,23 +1,19 @@
-from decouple import config
-from telethon.sync import TelegramClient
-from telethon import functions, types
-from api.models import INTERFACE_TELEGRAM, LANGUAGE_EN
-
-TELEGRAM_API_ID = config("TELEGRAM_API_ID", cast=int)
-TELEGRAM_API_HASH = config("TELEGRAM_API_HASH")
+from telethon import TelegramClient
+from datetime import datetime
+from datetime import timezone
+import asyncio
+import configparser
 
 
-def search_telegram_messages(
-    chat_name=None, search_term=None, min_date=None, max_date=None
-):
+def get_messages_sync(searchterm, chat_name, start_date, end_date):
     """Get Messages from Telegram, will block until async calls complete.
 
     Parameters
     ----------
+    searchterm: str
+        Term to search.
     chat_name: str, optional
-        Name of the chat to search in.
-    search_term: str, optional
-        Term to search, can be None as well.
+        Name of the chat to search in, can be None as well for global search.
     start_date: datetime, optional
         Earliest date of potential matched messages
     end_date: datetime, optional
@@ -25,122 +21,115 @@ def search_telegram_messages(
 
     Returns
     -------
-    results: list of dicts
-        Matched telegram messages.
+    messages: Telethon messages
+        Matched messages.
     """
-    if search_term is None:
-        search_term = ""
-    with TelegramClient("uanow", TELEGRAM_API_ID, TELEGRAM_API_HASH) as client:
-        n_messages_per_request = 100  # seems this is max number already
-        # Offset rate are needed for pagination/in case more than 100 messages
-        # should be retrieved
-        # offset rate is for global search
-        # offset id for chat/peer-specific search
-        # https://core.telegram.org/api/offsets
-        # https://core.telegram.org/constructor/messages.messagesSlice
-        offset_rate = 0
-        offset_id = 0
 
-        api_responses = []
-        while len(api_responses) == 0 or (
-            len(result.messages) == n_messages_per_request
-        ):
-            if chat_name is None:
-                result = client(
-                    functions.messages.SearchGlobalRequest(
-                        q=search_term,
-                        filter=types.InputMessagesFilterEmpty(),
-                        min_date=min_date,
-                        max_date=max_date,
-                        offset_id=0,  # offset_rate is used here
-                        offset_peer=types.InputPeerEmpty(),
-                        offset_rate=offset_rate,
-                        limit=n_messages_per_request,
+    loop = asyncio.get_event_loop()
+    messages = loop.run_until_complete(
+        get_messages_async(searchterm, chat_name, start_date, end_date)
+    )
+    return messages
+
+
+async def get_messages_async(searchterm, chat_name, start_date, end_date):
+    """Get Messages from Telegram, async version.
+
+    Parameters
+    ----------
+    searchterm: str
+        Term to search.
+    chat_name: str, optional
+        Name of the chat to search in, can be None as well for global search.
+    start_date: datetime, optional
+        Earliest date of potential matched messages
+    end_date: datetime, optional
+        Last date of potential matched messages
+
+    Returns
+    -------
+    messages: Telethon messages
+        Matched messages.
+    """
+    # reading config
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    # setting values
+    api_id = config["Telegram"]["api_id"]
+    api_hash = config["Telegram"]["api_hash"]
+
+    api_hash = str(api_hash)
+    api_id = int(api_id)
+    client = TelegramClient("session_id", api_id=api_id, api_hash=api_hash)
+    async with client:
+        # Client needs to see the chat we want to search in,
+        # here we try via get_dialogs
+        dialogs = await client.get_dialogs()
+        messages = await get_messages_from_client(
+            client, searchterm, chat_name, start_date, end_date
+        )
+    return messages
+
+
+async def get_messages_from_client(client, searchterm, chat_name, start_date, end_date):
+    """Get Messages from Telegram given the client.
+
+    Parameters
+    ----------
+    client: Telethon client
+    searchterm: str
+        Term to search.
+    chat_name: str, optional
+        Name of the chat to search in, can be None as well for global search.
+    start_date: datetime, optional
+        Earliest date of potential matched messages
+    end_date: datetime, optional
+        Last date of potential matched messages
+
+    Returns
+    -------
+    messages: Telethon messages
+        Matched messages.
+    """
+    # We loop through messages until we reach the start date
+    # or until there are no more matching messages
+    start_date_reached = False
+    messages = []
+    async with client:
+        # 20 is the limit on how many messages to fetch at once.
+        # Remove or change for more.
+        # Not sure if this extra buffering is even necessary
+        n_buffer_messages = 20
+        # In each iteration we go further back in time by setting end_date to date of last returned message
+        # (messages are returned from newest to oldest message)
+        while not start_date_reached:
+            this_messages = []
+            async for msg in client.iter_messages(
+                chat_name, n_buffer_messages, search=searchterm, offset_date=end_date
+            ):
+                if start_date is not None:
+                    start_date_reached = msg.date < start_date.replace(
+                        tzinfo=msg.date.tzinfo
                     )
-                )
-                offset_rate = result.next_rate
-            else:
-                result = client(
-                    functions.messages.SearchRequest(
-                        peer=chat_name,
-                        q=search_term,
-                        filter=types.InputMessagesFilterEmpty(),
-                        min_date=min_date,
-                        max_date=max_date,
-                        offset_id=offset_id,
-                        add_offset=0,
-                        limit=n_messages_per_request,
-                        max_id=0,
-                        min_id=0,
-                        hash=0,
-                        from_id=None,
-                    )
-                )
-                if len(result.messages) > 0:
-                    offset_id = result.messages[-1].id
-            api_responses.append(result)
-    results = []
-    for result in api_responses:
-        # Create dictionary of  channel/user ids to names for lookup
-        ch_ids_to_name = {c.id: c.username for c in result.chats}
-        user_ids_to_name = {u.id: u.username for u in result.users}
-        for msg in result.messages:
-            if hasattr(msg.peer_id, "channel_id"):
-                origin = ch_ids_to_name[msg.peer_id.channel_id]
-            else:
-                origin = user_ids_to_name[msg.peer_id.user_id]
-            results.append(
-                dict(
-                    timestamp=msg.date,
-                    text=msg.message,
-                    interface=INTERFACE_TELEGRAM,
-                    language=LANGUAGE_EN,
-                    origin=origin,
-                )
-            )
-    return results
+                if start_date_reached:
+                    break
+                this_messages.append(msg)
+            if len(this_messages) > 0:
+                end_date = msg.date
+            start_date_reached = start_date_reached or (len(this_messages) == 0)
+            messages.extend(this_messages)
+    return messages
 
 
 if __name__ == "__main__":
-    import datetime as dt
-    from backports import zoneinfo
-
-    TZ_UTC = zoneinfo.ZoneInfo("UTC")
-
-    print("No search term or chat name")
-    result = search_telegram_messages(
-        chat_name=None,
-        search_term=None,
-        min_date=dt.datetime(2022, 4, 11, 23, 0, 0, tzinfo=TZ_UTC),
-        max_date=dt.datetime(2022, 4, 12, 0, 0, 0, tzinfo=TZ_UTC),
-    )
-    print(len(result))
-    print(result)
-
-    print("No min max date term")
-    result = search_telegram_messages(
-        chat_name="t.me/ukrainearmyforce",
-        search_term="Харків",
-    )
-    print(len(result))
-    print(result)
-
-    print("No search term")
-    result = search_telegram_messages(
-        chat_name="t.me/ukrainearmyforce",
-        search_term=None,
-        min_date=dt.datetime(2022, 4, 11, 0, 0, 0, tzinfo=TZ_UTC),
-        max_date=dt.datetime(2022, 4, 12, 0, 0, 0, tzinfo=TZ_UTC),
-    )
-    print(len(result))
-    print(result)
-
-    print("Everything given")
-    result = search_telegram_messages(
-        chat_name="t.me/ukrainearmyforce",
-        search_term="Харків",
-        min_date=dt.datetime(2022, 4, 11, 0, 0, 0, tzinfo=TZ_UTC),
-        max_date=dt.datetime(2022, 4, 12, 0, 0, 0, tzinfo=TZ_UTC),
-    )
-    print(len(result))
-    print(result)
+    chat_name = "Ukraine NOW [English]"  # UkraineNow English public group
+    searchterm = "mykolaiv"
+    end_date = datetime(2022, 4, 8)
+    start_date = datetime(2022, 3, 20)
+    messages = get_messages_sync(searchterm, chat_name, start_date, end_date)
+    for msg in messages:
+        print(msg.date)
+        if msg.text:
+            print(msg.text)
+            print("\n")
